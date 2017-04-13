4e092fc2de583fcfef21384a8d1eb1811f2b0985
Suyi Diao <suyi.diao@vanderbilt.edu>
2017-04-12 21:21:31 -0500

    first commit

---
+++ driver.js
@@ -0,0 +1,29 @@
+/**
+ * Created by diaosuyi on 4/12/17.
+ */
+// let FileAPI = require('file-api')
+//     , File = FileAPI.File
+//     , FileList = FileAPI.FileList
+//     , FileReader = FileAPI.FileReader
+//     ;
+let fs = require('fs'),
+    readline = require('readline');
+
+/************************************************************/
+// Main
+// Setup the sudoku board
+let sudokuBoard = [];
+
+// Read file synchronously
+let lines = fs.readFileSync('./sudoku.txt', 'utf-8')
+    .split("\n")
+    .filter(Boolean);
+
+// Map the read lines into sudoku board
+lines.forEach(line => {
+    sudokuBoard.push(line.split(' ').map(x => {
+        return parseInt(x);
+    }))
+});
+
+// Ready to go
